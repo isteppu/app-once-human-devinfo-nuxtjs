e1688de77b2f178b8f612129b0847e8d073be9fd
isteppu <stefanieasaytono@gmail.com>
2025-09-08 15:26:58 +0800

    Added scenario, and silos in server

--- server/api/scenarios/[...].js
+++ server/api/scenarios/[...].js
@@ -9,9 +9,9 @@ export default defineEventHandler(async (event) => {
 	const params = getRouterParams(event);
 	const path = params.path.split('/');
 
-	if (path[0] === 'needs') {
+	if (path[0] === 'droptype') {
 		const id = path[1] === 'id' ? path[2] : null;
-		const tableName = 'DeviationNeeds';
+		const tableName = 'ScenarioDropType';
 
 		switch (method) {
 			case 'get':
@@ -27,27 +27,9 @@ export default defineEventHandler(async (event) => {
 			default:
 				throw createError({ statusCode: 405, statusMessage: 'Method Not Allowed' });
 		}
-	} else if (path[0] === 'types') {
-		const id = path[1] === 'id' ? path[2] : null;
-		const tableName = 'DeviationTypes';
-
-		switch (method) {
-			case 'get':
-				return handleDatabaseOperation(tableName, 'get', id);
-			case 'post':
-				const postData = await readBody(event);
-				return handleDatabaseOperation(tableName, 'post', null, postData);
-			case 'put':
-				const putData = await readBody(event);
-				return handleDatabaseOperation(tableName, 'put', id, putData);
-			case 'delete':
-				return handleDatabaseOperation(tableName, 'delete', id);
-			default:
-				throw createError({ statusCode: 405, statusMessage: 'Method Not Allowed' });
-		}
-	} else if (path[0] === 'id') {
+	}  else if (path[0] === 'id') {
 		const id = path[1];
-		const tableName = 'Deviations';
+		const tableName = 'Scenario';
 
 		switch (method) {
 			case 'get':
@@ -61,7 +43,7 @@ export default defineEventHandler(async (event) => {
 				throw createError({ statusCode: 405, statusMessage: 'Method Not Allowed' });
 		}
 	} else if (path.length === 1 && path[0] === '') {
-		const tableName = 'Deviations';
+		const tableName = 'Scenario';
 		switch (method) {
 			case 'get':
 				return handleDatabaseOperation(tableName, 'get');
